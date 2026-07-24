9eaeaf27ca8071fdd5ad150f520b49a240754c61
Famiton <you@example.com>
2026-07-24 21:50:01 +0300

    init

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_gsi_creek.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_gsi_creek-user \
+    lineage_gsi_creek-userdebug \
+    lineage_gsi_creek-eng
